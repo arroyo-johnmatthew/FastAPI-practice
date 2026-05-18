1585fb3c5fe8d1cb3a852f525178a55f40e79e5b
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-05-18 22:16:44 +0800

    feat: use shuffle instead of choices in randomizing bullet
    this is done to replicate real life russion roulette (the less bullets,
    the more chances you will shoot yourself with a live bullet)

--- hobby-projects/russian_roulette.py
+++ hobby-projects/russian_roulette.py
@@ -1,38 +1,30 @@
 import random
 
 # one live bullet in a gun
-gun = 6
-trigger = [0, 1]
+chamber = [0, 0, 0, 0, 0, 1]
+
+# spins the chamber once (will spin again if the program runs again)
+random.shuffle(chamber)
 
 while True:
     choice = input("🔫 Fire a bullet? yes/no: ").lower()
     print("")
 
     if choice in ["no", "n"]:
-        print("Goodbye!")
+        print("Goodbye!\n")
         break
 
     elif choice in ["yes", "y"]:
-
-        # checks if the gun that will be fired contains 1 bullet left 
-        if gun == 1:
-            print("🪦  You just killed yourself!")
-            break
-
-        result = random.choice(trigger)
-
-        # checks if there are still bullets inside the gun
-        if gun == 0:
-            print("😁 You are now safe. The gun is empty")
-            break
-
-        # checks if the bullet fired is real
-        elif result == 1:
-            print("💀 YOU ARE DEAD 💀")
-            break
+        # picks a bullet from the chamber
+        bullet = random.randrange(len(chamber))
     
+        # store it in a variable to check if the bullet fired is real or not
+        shot = chamber.pop(bullet)
+
+        if shot == 1:
+            print("💀 You are Dead 💀\n")
+            break
         else:
-            print("✅ You are not dead")
-            # subtract the remaining bullets
-            gun -= 1
-            print(f"gun now {gun}\n")
+            print("You survived the shot 😰")
+            print(f"bullets remaining: {len(chamber)}\n")
+            continue
